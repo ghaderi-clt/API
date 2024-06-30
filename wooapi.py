0ebd37a33bed40457deabbf1daff37f1aed0a976
Yaser GHaderi <48249086+yaserghaderi@users.noreply.github.com>
2024-06-30 17:09:08 +0330

    Update wooapi.py

--- wooapi.py
+++ wooapi.py
@@ -3,8 +3,8 @@ from woocommerce import API
 
 wcapi = API(
     url="https://beautynani.com",
-    consumer_key="ck_bb294b831c0039a8a57c2f8058f00ed389ad72fb",
-    consumer_secret="cs_1fc1a20d24a8b235375a2aa01c9d564956823552",
+    consumer_key="ck_bb294b831c0039a8a57c2f8058f00ed389a",
+    consumer_secret="cs_1fc1a20d24a8b235375a2aa01c9d56495823552",
     version="wc/v3"
 )
 r= wcapi.get("products")
